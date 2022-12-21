3798bb815135ed57bf973d55c503f6bdb3d8cef1
Till Seifert <me@till.seifert.name>
2022-12-21 14:27:28 +0100

    Add interactive Oberleitung display

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-async function initMap() {
+async function initMap()
+{
   const latLong = [51.4, 10.4];
 
   const map = L.map('map').setView(latLong, 6);
@@ -6,31 +7,34 @@ async function initMap() {
   proj4.defs(
     'EPSG:3034',
     '+proj=lcc +lat_0=52 +lon_0=10 +lat_1=35 +lat_2=65 +x_0=4000000 +y_0=2800000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
-  ); 
+  );
+  proj4.defs("EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs");
+
+  const hintergundLayer = L.layerGroup([]).addTo(map);
 
   // add german border
-  /* 
-  const outlineGermany = await (
-    await fetch(
-      './data/Hintergrundkarte/Grenze%20Deutschland.geojson'
-    )
-  ).json();
+  /*
+   const outlineGermany = await (
+   await fetch(
+   './data/Hintergrundkarte/Grenze%20Deutschland.geojson'
+   )
+   ).json();
 
-  L.geoJSON(outlineGermany, {
-    style: { color: '#333' },
-  }).addTo(map);
-  */
+   L.geoJSON(outlineGermany, {
+   style: { color: '#333' },
+   }).addTo(map);
+   */
 
   // add state backgrounds
   const outlineStates = await (
     await fetch(
-      './data/Hintergrundkarte/Grenze%20Bundesländer.geojson' 
+      './data/Hintergrundkarte/Grenze%20Bundesländer.geojson'
     )
   ).json();
 
   L.geoJSON(outlineStates, {
-    style: { color: '#333', weight:1, stroke:false },
-  }).addTo(map);
+    style: { color: '#333', weight: 1, stroke: false },
+  }).addTo(hintergundLayer);
 
   // add german highways
   const highways = await (
@@ -40,15 +44,15 @@ async function initMap() {
   ).json();
 
   L.Proj.geoJson(highways, {
-    style: (f) => ({ color: '#888' }),
-    fill: false,
+    style:  _ => ({ color: '#888' }),
+    fill:   false,
     stroke: '#777',
-  }).addTo(map);
+  }).addTo(hintergundLayer);
 
   // state borders
   L.geoJSON(outlineStates, {
-    style: { color: '#333', weight:1, fill:false },
-  }).addTo(map);
+    style: { color: '#333', weight: 1, fill: false },
+  }).addTo(hintergundLayer);
 
   // add city dots
   const citiesGermany = await (
@@ -58,28 +62,84 @@ async function initMap() {
   ).json();
 
   const geojsonMarkerOptions = {
-    radius: 4,
-    fillColor: '#eee',
-    color: '#000',
-    weight: 1,
-    opacity: 1,
+    radius:      4,
+    fillColor:   '#EEE',
+    color:       '#000',
+    weight:      1,
+    opacity:     1,
     fillOpacity: 0.8,
   };
 
   L.geoJSON(citiesGermany, {
-    pointToLayer: function (feature, latlng) {
-      return (
-        L.circleMarker(latlng, geojsonMarkerOptions)
-          // add label to each city dot
-          .bindTooltip(feature.properties.name, {
-            permanent: true,
-            direction: 'right',
-            className: 'city',
-          })
-          .openTooltip()
-      );
+    pointToLayer: (feature, latlng) => L.circleMarker(latlng, geojsonMarkerOptions)
+      // add label to each city dot
+      .bindTooltip(feature.properties.name, {
+        permanent: true,
+        direction: 'right',
+        className: 'city',
+      })
+      .openTooltip(),
+  }).addTo(hintergundLayer);
+
+
+  // =========
+
+  // add oberleitung
+  const oberleitung = await (
+    await fetch(
+      './data/Oberleitungsausbau.geojson'
+    )
+  ).json();
+
+  const oberleitungMarkerOptions = {
+    radius:      3000, // in meters here
+    fillColor:   '#EDA',
+    stroke:      false,
+    fillOpacity: 1,
+  };
+
+  const oberleitungsLayer = L.layerGroup([]).addTo(map);
+
+  const oberleitungsFeatures = new Map();
+
+  L.Proj.geoJson(oberleitung, {
+    pointToLayer: (feature, latlng) => {
+      const marker = L.circle(latlng, oberleitungMarkerOptions);
+      oberleitungsFeatures.set(feature, marker);
+      return marker;
     },
-  }).addTo(map);
+    style:        _ => ({ color: '#888' }),
+    fill:         false,
+    stroke:       '#777',
+  }).addTo(oberleitungsLayer);
+
+
+  // interactive controls
+
+
+  const yearSlider = document.querySelector('#input_year');
+
+  function updateOberleitungslayer()
+  {
+    const year = yearSlider.valueAsNumber;
+
+    oberleitungsFeatures.forEach((marker, feature) => {
+      // check if feature is before the selected year
+      const featureDate = new Date(feature.properties?.Time);
+      if(featureDate.getFullYear() <= year) {
+        marker.addTo(oberleitungsLayer)
+      }
+      else {
+        marker.remove()
+      }
+    });
+  };
+  updateOberleitungslayer();
+
+  yearSlider.addEventListener('input', ev => {
+    updateOberleitungslayer();
+  });
 }
 
 initMap();
+
