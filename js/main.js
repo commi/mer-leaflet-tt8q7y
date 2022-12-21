dcb6e8ae6b281665384d60206aaf92083c657f6b
commi <me@till.seifert.name>
2022-12-21 13:04:58 +0100

    set layer order

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ async function initMap() {
   proj4.defs(
     'EPSG:3034',
     '+proj=lcc +lat_0=52 +lon_0=10 +lat_1=35 +lat_2=65 +x_0=4000000 +y_0=2800000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
-  );
+  ); 
 
   // add german border
+  /* 
   const outlineGermany = await (
     await fetch(
       'https://raw.githubusercontent.com/commi/mer-leaflet-tt8q7y/main/data/Hintergrundkarte/Grenze%20Deutschland.geojson'
@@ -18,15 +19,35 @@ async function initMap() {
   L.geoJSON(outlineGermany, {
     style: { color: '#333' },
   }).addTo(map);
+  */
 
+  // add state backgrounds
   const outlineStates = await (
     await fetch(
-      'https://raw.githubusercontent.com/commi/mer-leaflet-tt8q7y/main/data/Hintergrundkarte/Grenze%20Bundesländer.geojson'
+      'https://raw.githubusercontent.com/commi/mer-leaflet-tt8q7y/main/data/Hintergrundkarte/Grenze%20Bundesländer.geojson' 
     )
   ).json();
 
   L.geoJSON(outlineStates, {
-    style: { color: '#333' },
+    style: { color: '#333', weight:1, stroke:false },
+  }).addTo(map);
+
+  // add german highways
+  const highways = await (
+    await fetch(
+      'https://raw.githubusercontent.com/commi/mer-leaflet-tt8q7y/main/data/Hintergrundkarte/TEN-T%20roads.geojson'
+    )
+  ).json();
+
+  L.Proj.geoJson(highways, {
+    style: (f) => ({ color: '#888' }),
+    fill: false,
+    stroke: '#777',
+  }).addTo(map);
+
+  // state borders
+  L.geoJSON(outlineStates, {
+    style: { color: '#333', weight:1, fill:false },
   }).addTo(map);
 
   // add city dots
@@ -58,19 +79,6 @@ async function initMap() {
           .openTooltip()
       );
     },
-  }).addTo(map);
-
-  // add german highways
-  const highways = await (
-    await fetch(
-      'https://raw.githubusercontent.com/commi/mer-leaflet-tt8q7y/main/data/Hintergrundkarte/TEN-T%20roads.geojson'
-    )
-  ).json();
-
-  L.Proj.geoJson(highways, {
-    style: (f) => ({ color: '#888' }),
-    fill: false,
-    stroke: '#777',
   }).addTo(map);
 }
 
