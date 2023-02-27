7052ab6404978dd91c596d43b7390448af76afd0
Till Seifert <me@till.seifert.name>
2023-02-27 10:54:15 +0100

    Update for new GeoJSON files
    - generify layer creation

--- js/main.js
+++ js/main.js
@@ -1,80 +1,78 @@
+/** Function to calculate the color of a feature based on a number
+ *
+ * @return {string}
+ * @param {string} lowColor
+ * @param {string} highColor
+ * @param {number} minWert
+ * @param {number} maxWert
+ * @param {number} wert
+ */
+function getLineColor(lowColor, highColor, minWert, maxWert, wert)
+{
+  // Calculate the proportion of the value between the minimum and maximum values
+  const proportion = (wert - minWert) / (maxWert - minWert);
+
+  // Interpolate between the two colors based on the proportion, return the color as a string
+  return chroma.interpolate(lowColor, highColor, proportion, 'hsl').hex();
+}
+
+
 async function initMap()
 {
-  const latLong = [51.4, 10.4];
+  // create LeafLet map on the div #map and center it on germany
+  const map = L.map('map').setView([51.4, 10.4], 6);
 
-  const map = L.map('map').setView(latLong, 6);
-
-  proj4.defs(
-    'EPSG:3034',
-    '+proj=lcc +lat_0=52 +lon_0=10 +lat_1=35 +lat_2=65 +x_0=4000000 +y_0=2800000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
-  );
-  proj4.defs("EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs");
-
+  // define projection definition used in the GeoJSOn files
+  proj4.defs('EPSG:3034',
+    '+proj=lcc +lat_0=52 +lon_0=10 +lat_1=35 +lat_2=65 +x_0=4000000 +y_0=2800000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs');
+  proj4.defs('EPSG:25832',
+    "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs");
+  // OGC:CRS84 is the same as EPSG:4326
   proj4.defs['OGC:CRS84'] = proj4.defs['EPSG:4326'];
 
+  // add layer to map for holding the background sub-layers
   const hintergundLayer = L.layerGroup([]).addTo(map);
 
-  // add german border
-  /*
-   const outlineGermany = await (
-   await fetch(
-   './data/Hintergrundkarte/Grenze%20Deutschland.geojson'
-   )
-   ).json();
+  // Add german state backgrounds
+  // fetch GeoJOSN file and decode it as JSON
+  const outlineStates = await (await fetch('./data/Hintergrundkarte/Grenze%20Bundesländer.geojson')).json();
 
-   L.geoJSON(outlineGermany, {
-   style: { color: '#333' },
-   }).addTo(map);
-   */
-
-  // add state backgrounds
-  const outlineStates = await (
-    await fetch(
-      './data/Hintergrundkarte/Grenze%20Bundesländer.geojson'
-    )
-  ).json();
-
+  // Add german state backgrounds to background layer
   L.geoJSON(outlineStates, {
     style: { color: '#333', weight: 1, stroke: false },
   }).addTo(hintergundLayer);
 
   // add german highways
-  const highways = await (
-    await fetch(
-      './data/Hintergrundkarte/TEN-T%20roads.geojson'
-    )
-  ).json();
+  // fetch GeoJOSN file and decode it as JSON
+  const highways = await (await fetch('./data/Hintergrundkarte/TEN-T%20roads.geojson')).json();
 
+  // Add german highways to background layer
   L.Proj.geoJson(highways, {
     style:  _ => ({ color: '#888' }),
     fill:   false,
     stroke: '#777',
   }).addTo(hintergundLayer);
 
-  // state borders
+  // Add german state border to background layer again. this time the border as black lines, this is done here to draw the lines over the highways
   L.geoJSON(outlineStates, {
     style: { color: '#333', weight: 1, fill: false },
   }).addTo(hintergundLayer);
 
   // add city dots
-  const citiesGermany = await (
-    await fetch(
-      './data/Hintergrundkarte/St%C3%A4dte%20Deutschland.geojson'
-    )
-  ).json();
+  // fetch GeoJOSN file and decode it as JSON
+  const citiesGermany = await (await fetch('./data/Hintergrundkarte/St%C3%A4dte%20Deutschland.geojson')).json();
 
-  const geojsonMarkerOptions = {
-    radius:      4,
-    fillColor:   '#EEE',
-    color:       '#000',
-    weight:      1,
-    opacity:     1,
-    fillOpacity: 0.8,
-  };
-
+  // Add cities to background layer
   L.geoJSON(citiesGermany, {
-    pointToLayer: (feature, latlng) => L.circleMarker(latlng, geojsonMarkerOptions)
-      // add label to each city dot
+    pointToLayer: (feature, latlng) => L.circleMarker(latlng, {
+        radius:      4,
+        fillColor:   '#EEE',
+        color:       '#000',
+        weight:      1,
+        opacity:     1,
+        fillOpacity: 0.8,
+      })
+      // add a label to each city dot
       .bindTooltip(feature.properties.name, {
         permanent: true,
         direction: 'right',
@@ -86,100 +84,135 @@ async function initMap()
 
   // =========
 
-  // add oberleitung
-  const oberleitung = await (
-    await fetch(
-      './data/Oberleitungsausbau.geojson'
-    )
-  ).json();
-
-  const oberleitungMarkerOptions = {
-    radius:      3000, // in meters here
-    fillColor:   '#EDA',
-    stroke:      false,
-    fillOpacity: 1,
-  };
-
-  const oberleitungsLayer = L.layerGroup([]).addTo(map);
-
+  // add oberleitung layer
+  const oberleitung          = await (await fetch('./data/Oberleitungsausbau.geojson')).json();
+  // add a layer to the existing map
+  const oberleitungsLayer    = L.layerGroup([]).addTo(map);
+  // Map to store features and the map objects created from them outside that map to manipulate them later
   const oberleitungsFeatures = new Map();
 
   L.Proj.geoJson(oberleitung, {
-    pointToLayer: (feature, latlng) => {
-      const marker = L.circle(latlng, oberleitungMarkerOptions);
-      oberleitungsFeatures.set(feature, marker);
-      return marker;
+    pointToLayer:  (feature, latlng) => L.circle(latlng, {
+      radius:      3000, // in meters here
+      fillColor:   '#EDA',
+      stroke:      false,
+      fillOpacity: 1,
+    }),
+    onEachFeature: (feature, layer) => {
+      // add each new layer to the Map, linked with the feature it was created from
+      oberleitungsFeatures.set(feature, layer);
     },
-    style:        _ => ({ color: '#888' }),
-    fill:         false,
-    stroke:       '#777',
+    style:         _ => ({ color: '#888' }),
+    fill:          false,
+    stroke:        '#777',
   }).addTo(oberleitungsLayer);
 
-  // add Diesel
-  const DieselFahrten = await (
-    await fetch(
-      './data/GeoJSON/Diesel.geojson'
-    )
-  ).json();
 
-  const DieselFahrtenLayer = L.layerGroup([]).addTo(map);
+  /**
+   * Function to add a GeoJSON file with MultiLineFeatures to the map.
+   * It creates a layer and adds all features as sub-layers to it.
+   * It colors the with the value derived with {@see getValueFromFeature}
+   *
+   * @param {string} url URL to GeoJSON file
+   * @param {string} lowColor      Define the colors to use for the gradient
+   * @param {string} highColor      Define the colors to use for the gradient
+   * @param {number} minValue       Define the minimum and maximum values of the Diesel_Wer property
+   * @param {number} maxValue       Define the minimum and maximum values of the Diesel_Wer property
+   * @param {(Object) => Number} getValueFromFeature  function to get a scalar value from the GeoJSON feature
+   */
+  async function addLayer(url, lowColor, highColor, minValue, maxValue, getValueFromFeature)
+  {
+    // fetch the File and decode it as JSON
+    const json        = await (await fetch(url)).json();
+    // add a layer to the existing map
+    const parentLayer = L.layerGroup([]).addTo(map);
+    // Map to store features and the map objects created from them outside that map to manipulate them later
+    const features    = new Map();
 
-  const DieselFahrtenFeatures = new Map();
+    // create map layers from GeoJSON
+    L.Proj.geoJson(json, {
+      style:         feature => ({
+        // set color of the feature with the value of the Diesel_Wer property
+        "color": getLineColor(lowColor, highColor, minValue, maxValue, getValueFromFeature(feature))
+      }),
+      onEachFeature: (feature, layer) => {
+        // add each new layer to the Map, linked with the feature it was created from
+        features.set(feature, layer);
+      },
+      fill:          false,
+    }).addTo(parentLayer);
+    return { parentLayer, features };
+  }
 
-  L.Proj.geoJson(DieselFahrten, {
-    pointToLayer: (feature, latlng) => {
-      if(feature.properties.Wert) {
-        // convert german date to iso date
-        const isoDate     = feature?.properties?.Time.replace(/(\d\d)\.(\d\d)\.(\d{4}).*/, '$3-$2-$1');
-        // check if feature is before the selected year
-        const featureDate = new Date(isoDate);
+  // Map with names and layers that are added to the map
+  const layers = new Map();
 
-        if(featureDate.getFullYear() != 2023) return;
+  // create and add Diesel layer
+  layers.set('Diesel', await addLayer('./data/GeoJSON/Diesel.geojson', '#FFEDA0', '#800026', 0, 15000, feature => feature.properties?.Diesel_Wer ?? 0));
+  // create and add FCEV layer
+  layers.set('FCEV', await addLayer('./data/GeoJSON/FCEV.geojson', '#F7FCF5', '#075C05', 0, 15000, feature => feature.properties?.FCEV_Wert ?? 0));
 
-        const marker = L.circle(latlng, {
-          radius:      feature.properties.Wert / 2, // in meters here
-          fillColor:   'red',
-          stroke:      false,
-          fillOpacity: 0.5,
-        });
-        DieselFahrtenFeatures.set(feature, marker);
-        return marker;
-      }
-    },
-    style:        _ => ({ color: '#888' }),
-    fill:         false,
-    stroke:       '#777',
-  }).addTo(DieselFahrtenLayer);
+  // get references to controls from HTML
+  const yearSlider              = document.querySelector('#input_year');
+  const radioOberleitungsausbau = document.querySelector('#visible_layer_oberleitungsausbau');
 
-
-  // interactive controls
-  const yearSlider = document.querySelector('#input_year');
-
-
-  // show / hide Oberleitungen depending on seleted year
-  function updateOberleitungslayer()
+  // show / hide Layer depending on seleted year and selected checkboxes
+  function updateLayerVisibility()
   {
     const year = yearSlider.valueAsNumber;
 
+    // add/remove Oberleitungs-features to berleitungs-layer if the checkbox is checked and the year as before the feature date
     oberleitungsFeatures.forEach((marker, feature) => {
+      if(!radioOberleitungsausbau.checked) {
+        marker.remove();
+        return;
+      }
       // convert german date to iso date
-      const isoDate     = feature?.properties?.Time.replace(/(\d\d)\.(\d\d)\.(\d{4})/, '$3-$2-$1');
+      const isoDate = feature?.properties?.Time?.replace(/(\d\d)\.(\d\d)\.(\d{4})/, '$3-$2-$1');
       // check if feature is before the selected year
-      const featureDate = new Date(isoDate);
-      if(featureDate.getFullYear() <= year) {
+      if(new Date(isoDate).getFullYear() <= year) {
         marker.addTo(oberleitungsLayer)
       }
       else {
         marker.remove()
       }
     });
-  };
-  updateOberleitungslayer();
 
-  yearSlider.addEventListener('input', _ => {
-    updateOberleitungslayer();
+    // add/remove Diesel/FCEV/…-features to their respective layer if the checkbox is checked and the year matches the feature date
+    layers.forEach(({ features, parentLayer }, name) => {
+
+      // get the checked state of the checkbox associated with this layer with the matching 'data-toggles' attribut
+      let layerCheckboxChecked = document.querySelector(`input[data-toggles="${name}"]`).checked;
+
+      features?.forEach((layer, feature) => {
+        // if the checkbox is not checked, jsut remove the feature
+        if(!layerCheckboxChecked) {
+          layer.remove();
+          return;
+        }
+        // convert german date to iso date
+        const isoDate = feature?.properties?.Time?.replace(/(\d\d)\.(\d\d)\.(\d{4})/, '$3-$2-$1');
+        // check if feature is exaclty in the selected year
+        if(new Date(isoDate).getFullYear() === year) {
+          layer.addTo(parentLayer)
+        }
+        else {
+          layer.remove()
+        }
+      });
+
+    });
+  }
+
+  // Update the visibility of all feature once after creating everything so the map state mathes the inputs
+  updateLayerVisibility();
+
+  // Update the visibility of all feature whenever a control in the #form_settings container is changed by the user
+  document.querySelector('#form_settings').addEventListener('input', _ => {
+    updateLayerVisibility();
   });
 }
 
+// start the loading process
 initMap();
 
